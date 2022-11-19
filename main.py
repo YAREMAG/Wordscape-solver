91ca17303ad390cd1c38950baac14c1ce60eef1f
Yarema Ganushchak <58015167+YAREMAG@users.noreply.github.com>
2022-11-18 22:51:10 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import time
 import inflect
-p = inflect.engine()
 from english_words import english_words_set
 from itertools import permutations
 
-start_time = time.time()
+p = inflect.engine()
 
-question =input('What are the letters?')
+question = input('What are the letters?')
 
 keywords = [''.join(i) for i in permutations(question)]
 keywords_3 = [''.join(i) for i in permutations(question, 3)]
@@ -14,7 +12,6 @@ keywords_4 = [''.join(i) for i in permutations(question, 4)]
 keywords_5 = [''.join(i) for i in permutations(question, 5)]
 
 keywords = keywords_3 +keywords_4 +keywords_5+keywords
-
 real_words = []
 
 for x in keywords:
@@ -23,6 +20,3 @@ for x in keywords:
     if len(x)< 5:
       real_words.append(p.plural(x))
 print(real_words)
-end_time = time.time()
-total = round(end_time-start_time,2)
-print(f"total time {total}")
