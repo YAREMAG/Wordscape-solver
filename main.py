bc1597e8ddd576e6190968d9742f3916c3c3f9db
Yarema Ganushchak <58015167+YAREMAG@users.noreply.github.com>
2022-11-18 22:47:38 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-import requests
-import nltk
 import time
+import inflect
+p = inflect.engine()
 from english_words import english_words_set
-from itertools import permutations, combinations
-key = '17382436-3621-47df-aba5-5c12e228f123'
+from itertools import permutations
+
 start_time = time.time()
-# thesaurus key 79cac01e-87b1-418a-ab22-1152876f140a
-
-#url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/energetic?key='+key
-
-#url_loops = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/' + '''word''' + '''?key=''' + key
-
-
-#p = requests.get(url).json()
-#print(p)
-#print(p[0]['def'][0]['sseq'][0][0][1]['dt'])  #THIS IS NEEDED TO GET TO THE WORD DEFINITION
-
-#ABOVE DOES NOT WORK IF THERE IS MORE THAN ONE VARIATION OF THE WORD
-#methodology for creating all possible words
 
 question =input('What are the letters?')
 
@@ -33,6 +20,8 @@ real_words = []
 for x in keywords:
   if x in english_words_set:
     real_words.append(x)
+    if len(x)< 5:
+      real_words.append(p.plural(x))
 print(real_words)
 end_time = time.time()
 total = round(end_time-start_time,2)
